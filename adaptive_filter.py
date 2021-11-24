2db2256c9601268ce619955f155aefd02a26f62e
angseung-home <angseung@keti.re.kr>
2021-11-24 23:57:38 +0900

    Refactored main code...

--- adaptive_filter.py
+++ adaptive_filter.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 
 class LMS:
-    def __init__(self, Wt, damp=0.5):
+    def __init__(self, Wt, step_size=0.5):
         self.Wt = np.squeeze(getattr(Wt, "A", Wt))
-        self.damp = damp
+        self.step_size = step_size
 
     def est(self, X, y, mode="prediction"):
 
@@ -15,19 +15,18 @@ class LMS:
             X = np.squeeze(getattr(X, "A", X))
             yest = self.Wt.dot(X)
             c = (y - yest) / X.dot(X)
-            self.Wt += self.damp * c * X
+            self.Wt += self.step_size * c * X
 
         elif mode == "cancelation":
             yest = self.Wt * X
             e = y - yest
             c = e.dot(e) / X.dot(X)
-            self.Wt += self.damp * c * X
+            self.Wt += self.step_size * c * X
 
         return yest
 
 
 def chirp(n, f0=2, f1=40, t1=1):
-    # from $scipy/signal/waveforms.py
     t = np.arange(n + 0.0) / n * t1
 
     return scipy.signal.chirp(t, f0, t1, f1)
@@ -40,48 +39,48 @@ plot = 3
 if __name__ == "__main__":
     for mode in ["prediction", "cancelation"]:
         if mode == "prediction":
-            filterlen = 10
-            damp = 0.05
-            nx = 2000
-            f1 = 40  # chirp
-            noise = 0.05 * 2  # * swing
+            filter_len = 10
+            step_size = 0.05
+            signal_len = 2000
+            f1 = 40
+            noise_power = 0.05 * 2
             seed = 0
 
             np.random.seed(seed)
 
-            Xlong = chirp(nx, f1=f1)
-            # Xlong = np.cos( 2*np.pi * freq * np.arange(nx) )
-            if noise:
-                Xlong += np.random.normal(scale=noise, size=nx)  # laplace ...
-            Xlong *= 10
+            signal_orig = chirp(signal_len, f1=f1)
 
-            ys = []
+            if noise_power:
+                signal_orig += np.random.normal(scale=noise_power, size=signal_len)
+
+            signal_orig *= 10
+
+            targets = []
             yests = []
 
-            lms = LMS(np.zeros(filterlen), damp=damp)
-            for t in range(nx - filterlen):
-                X = Xlong[t : t + filterlen]
-                y = Xlong[t + filterlen]  # predict
-                yest = lms.est(X, y, mode=mode)
-                ys += [y]
-                yests += [yest]
+            lms = LMS(np.zeros(filter_len), step_size=step_size)
 
-            y = np.array(ys)
-            yest = np.array(yests)
-            err = yest - y
+            for t in range(signal_len - filter_len):
+                X = signal_orig[t: t + filter_len]
+                target = signal_orig[t + filter_len]  # predict
+                y_est = lms.est(X, target, mode=mode)
+                targets += [target]
+                yests += [y_est]
+
+            target = np.array(targets)
+            y_est = np.array(yests)
+            error = y_est - target
 
             if plot:
                 fig = plt.figure(figsize=(8, 7))
-                # fig.set_size_inches(12, 8)
-                # fig.suptitle("Signal prediction", fontsize=12)
                 plt.subplot(211)
-                plt.plot(y, "k--", label="target")
+                plt.plot(target, "k--", label="target")
                 plt.title("Estimatied signal")
-                plt.plot(yest, label="predicted value")
+                plt.plot(y_est, label="predicted value")
                 plt.legend()
                 plt.grid(True)
                 plt.subplot(212)
-                plt.plot(err, label="error")
+                plt.plot(error, label="error")
                 plt.axhline(0, color="k", linestyle="--")
                 plt.title("prediction error")
                 plt.legend()
@@ -92,61 +91,57 @@ if __name__ == "__main__":
                 plt.show()
 
         elif mode == "cancelation":
-
-            filterlen = 10
-            damp = 0.1
-            nx = 2000
-            f1 = 40  # chirp
-            noise = 0.05 * 4  # * swing
+            filter_len = 10
+            step_size = 0.1
+            signal_len = 2000
+            f1 = 40
+            noise_power = 0.05 * 4
             seed = 0
 
             np.random.seed(seed)
-            t = np.arange(nx + 0.0) / nx
 
-            Xlong = chirp(nx, f1=f1)
-            # Xlong = np.cos( 2*np.pi * freq * np.arange(nx) )
+            t = np.arange(signal_len + 0.0) / signal_len
 
-            if noise:
-                Xlong_noise = (
-                    Xlong
-                    + 0.05 * np.cos(2 * np.pi * (f1 / 4) * t)
-                    + 0.2 * np.random.normal(scale=noise, size=nx)
-                )  # laplace ...
+            signal_orig = chirp(signal_len, f1=f1)
 
-            Xlong *= 10
-            Xlong_noise *= 10
+            if noise_power:
+                noised_sig = (
+                        signal_orig
+                        + 0.05 * np.cos(2 * np.pi * (f1 / 4) * t)
+                        + 0.2 * np.random.normal(scale=noise_power, size=signal_len)
+                )
 
-            ys = []
+            signal_orig *= 10
+            noised_sig *= 10
+
+            targets = []
             yests = []
-            iters = nx // filterlen
-            lms = LMS(np.zeros(filterlen), damp=damp)
+            iters = signal_len // filter_len
+            lms = LMS(np.zeros(filter_len), step_size=step_size)
 
             for t in range(iters):
-                X = Xlong_noise[filterlen * t : filterlen * (t + 1)]
-                y = Xlong[filterlen * t : filterlen * (t + 1)]
-                # y = X_hf[filterlen * t : filterlen * (t + 1)]
-                yest = lms.est(X, y, mode=mode)
-                ys += [y]
-                yests += [yest]
+                X = noised_sig[filter_len * t: filter_len * (t + 1)]
+                target = signal_orig[filter_len * t: filter_len * (t + 1)]
+                y_est = lms.est(X, target, mode=mode)
+                targets += [target]
+                yests += [y_est]
 
-            y = np.array(ys).flatten()
-            yest = np.array(yests).flatten()
-            err = yest - y
+            target = np.array(targets).flatten()
+            y_est = np.array(yests).flatten()
+            error = y_est - target
 
             if plot:
                 fig = plt.figure(figsize=(8, 7))
-                # fig.set_size_inches(12, 8)
-                # fig.suptitle("Signal prediction", fontsize=12)
                 plt.subplot(211)
-                # plt.plot(Xlong_noise)
-                plt.plot(yest, label="predicted value")
-                # plt.plot(Xlong_noise, "r--", label="noised")
-                plt.plot(Xlong, "k--", label="target")
+                plt.plot(noised_sig)
+                plt.plot(y_est, label="predicted value")
+                plt.plot(noised_sig, "r--", label="noised")
+                plt.plot(signal_orig, "k--", label="target")
                 plt.title("Cancelated signal")
                 plt.legend(loc="lower left")
                 plt.grid(True)
                 plt.subplot(212)
-                plt.plot(err, label="error")
+                plt.plot(error, label="error")
                 plt.axhline(0, color="k", linestyle="--")
                 plt.title("prediction error")
                 plt.legend(loc="lower left")
